9c8e7f37bba4ce4a164a9bd1a8af1b495841a1e4
System <none>
2017-04-15 08:51:02 +0000

    🏜🚵 Updated with Glitch

--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 // the simplest of maven servers
 
 var express = require('express');
-var fs = require('fs');
+const fs = require('fs-extra')
 var path = require('path')
 var getSize = require('get-folder-size');
+var bodyParser = require('body-parser')
 var app = express();
+
+//get bodies as buffers for octet-stream (wat)
+app.use(bodyParser.raw());
 
 //doesn't get copied between remixes https://glitch.com/faq/
 var folder = ".data";
@@ -25,6 +29,18 @@ app.get("/data", function (request, response) {
   });
 });
 
+app.get("/nuke", function (request, response) {
+    fs.remove(folder, err => {
+    if (err) {
+      console.error(err)
+      response.sendStatus(500);
+    }
+
+    console.log('they nuked everything!')
+    response.status(200).send("You just nuked all the artifacts")
+  })
+});
+
 //they request the file, we send it
 app.get("*", function (request, response) {
   var path = request.originalUrl;
@@ -42,14 +58,33 @@ app.get("*", function (request, response) {
 app.put("*", function (request, response) {
   var path = folder + request.originalUrl;
   console.log("Got a PUT request: " + path)
+  console.log("Content type: " + request.get("Content-Type"));
+  console.log("Putting:")
+  //we have to do some ugly stuff to get the buffer into a file. Brace yourself
+  var buffer = request.body;
   ensureDirectoryExistence(path);
+  fs.open(path, 'w', function(err, fd) {
+    if (err) {
+        console.log(err);
+        response.sendStatus(500);
+    }
+
+    fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+        if (err) { 
+          throw 'error writing file: ' + err;
+        }
+        fs.close(fd, function() {
+            console.log("The file was written!");
+            response.sendStatus(200);
+        })
+    });
+});
   fs.writeFile(path, request.body, function(err) {
     if(err) {
       console.log(err);
       response.sendStatus(500);
     } else {
-      console.log("The file was saved!");
-      response.sendStatus(200);
+      
     }
   }); 
   
