5588fe696f44008eec32637579ada2e93602b0a3
System <none>
2017-04-15 08:18:14 +0000

    🐲📻 Updated with Glitch

--- README.md
+++ README.md
@@ -1,27 +1,41 @@
-Welcome to the Glitch BETA
-=========================
+# fetch
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
+Private maven servers are complicated or pricey. fetch aims to make them simple and free. All you need to do is remix this project on glitch.com and do a bit of configuration.
 
-Glitch is a developer playground that lets you code a real web-app without the slow setup and deployment steps.
+## Configuration
+To make things private, you need to add a username and password to the `.env` file in Glitch. It'll look a little something like this:
+```
+# Environment Config
 
-[About Glitch](https://glitch.com/about)
+# store your secrets and config variables in here
+# only invited collaborators will be able to see your .env values
+
+# reference these in your code with process.env.SECRET
+
+USERNAME=blah
+PASSWORD=blah
+
+# note: .env is a shell file so there can't be spaces around =
+```
+Note that if you leave these blank, anyone will have read/write access to the maven artifacts.
+
+Now, within your Gradle project, you just need to configure a custom maven repo:
+```
+maven {
+  credentials {
+      username "your_username"
+      password "your_password"
+  }
+  url "https://yourproject.glitch.me"
+}
+```
+We recommend moving the username, password, and url out of the project itself and into a `gradle.properties` file in your Gradle HOME directory.
+
+## Limitataions
+glitch.com limits you to a max of 128 MB of files. You can check how many you are using by visiting `https://yourprojectname.glitch.me/data`
 
 
-Your Project
-------------
-
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env`
-
-On the front-end,
-- edit `client.js`, `style.css` and `index.html`
-- drag in `assets`, like images or music, to add them to your project
-
-
-Made by Fog Creek
+Made with [glitch.com](https://glitch.com/)
 -----------------
 
 \ ゜o゜)ノ
--- server.js
+++ server.js
@@ -1,38 +1,41 @@
 // server.js
-// where your node app starts
+// the simplest of maven servers
 
-// init project
 var express = require('express');
 var fs = require('fs');
 var path = require('path')
+var getSize = require('get-folder-size');
 var app = express();
 
-//doesn't get copied between remixes
+//doesn't get copied between remixes https://glitch.com/faq/
 var folder = ".data";
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.get("/", function (request, response) {
+  response.status(200).send("Nothing to see here, check out <a href=\"https://github.com/Commit451/fetch\">https://github.com/Commit451/fetch</a> instead")
+});
 
-//it all goes here
+app.get("/data", function (request, response) {
+  getSize(folder, function(err, size) {
+    if (err) { 
+      response.sendStatus(500);
+    } else {
+      console.log(size)
+      response.status(200).send((size / 1024 / 1024).toFixed(2) + ' Mb used')
+    }
+  });
+});
+
+//they request the file, we send it
 app.get("*", function (request, response) {
-  var path = folder + request.originalUrl;
+  var path = request.originalUrl;
   console.log("Got a GET request:" + path);
-  if (fs.existsSync(path)) {
-    fs.readFile(path, 'utf8', function (err,data) {
-      if (err) {
-        //204, assume the thing doesn't exist
-        response.sendStatus(400);
-        console.log(err);
-      } else {
-        console.log("Sending data:")
-        console.log(data);
-        response.status(200).send(data)
-      }
-    });
+  if (fs.existsSync(folder + path)) {
+    console.log("Found! Sending...")
+    response.sendFile(folder + path, { root: __dirname })
   } else {
+    console.log("Not found. Sending null")
     response.send(null);
   }
-  
 });
 
 //they send the file, we store it
@@ -53,7 +56,7 @@ app.put("*", function (request, response) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
