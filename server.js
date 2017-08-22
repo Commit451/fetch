4cf8bd4f7b353cf73b70cebd7495efed47c32f4e
John Carlson <jawnnypoo@gmail.com>
2017-08-22 09:16:23 -0500

    Send the raw json from dropbox if /view

--- server.js
+++ server.js
@@ -34,43 +34,54 @@ app.get("/", function (request, response) {
     response.status(200).send("Check out <a href=\"https://github.com/Commit451/fetch\">https://github.com/Commit451/fetch</a> to get started")
 });
 
+app.get("/view*", passport.authenticate('basic', {session: false}), function (request, response) {
+    let path = request.originalUrl;
+    path = path.replace("view", "maven");
+    console.log("Got a GET view request: " + path);
+    dbx.filesListFolder({path: path})
+        .then(function(res) {
+            console.log(res);
+            response.status(200).send(res);
+        })
+        .catch(function(error) {
+            console.error(error);
+            response.status(404).send();
+        });
+});
+
 //they request the file, we send it
-app.get("/maven*",
-    passport.authenticate('basic', {session: false}),
-    function (request, response) {
-        const path = request.originalUrl;
-        console.log("Got a GET request: " + path);
-        dbx.filesDownload({path: path})
-            .then(function (dbResponse) {
-                console.log(dbResponse);
-                response.status(200).send(dbResponse.fileBinary);
-            })
-            .catch(function (error) {
-                console.error(error);
-                response.send(null);
-            });
-    });
+app.get("/maven*", passport.authenticate('basic', {session: false}), function (request, response) {
+    const path = request.originalUrl;
+    console.log("Got a GET request: " + path);
+    dbx.filesDownload({path: path})
+        .then(function (dbResponse) {
+            console.log(dbResponse);
+            response.status(200).send(dbResponse.fileBinary);
+        })
+        .catch(function (error) {
+            console.error(error);
+            response.status(404).send(null);
+        });
+});
 
 //they send the file, we store it
-app.put("/maven*",
-    passport.authenticate('basic', {session: false}),
-    function (request, response) {
-        const path = request.originalUrl;
-        console.log("Got a PUT request: " + path);
+app.put("/maven*", passport.authenticate('basic', {session: false}), function (request, response) {
+    const path = request.originalUrl;
+    console.log("Got a PUT request: " + path);
 
-        const buffer = request.body;
+    const buffer = request.body;
 
-        dbx.filesUpload({path: path, contents: buffer, mode: 'overwrite'})
-            .then(function (dbResponse) {
-                response.sendStatus(200);
-                console.log(dbResponse);
-            })
-            .catch(function (error) {
-                console.error(error);
-                response.sendStatus(500);
-            });
+    dbx.filesUpload({path: path, contents: buffer, mode: 'overwrite'})
+        .then(function (dbResponse) {
+            response.sendStatus(200);
+            console.log(dbResponse);
+        })
+        .catch(function (error) {
+            console.error(error);
+            response.sendStatus(500);
+        });
 
-    });
+});
 
 // listen for requests :)
 const server = app.listen(process.env.PORT || '8080', function () {
